930e984dc2e1da98cb7e4c97063f5d9c3ecddecc
asknksk <asknkskk@gmail.com>
2022-06-07 11:04:46 +0300

    life zero: dissapper button

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let stop = 100;
 let start = 0;
 let guess = document.querySelector("#guess");
 const btn = document.querySelector("#btn");
+const resetBtn = document.querySelector("#reset-btn");
 const parag = document.querySelector("#parag");
 console.log(number);
 const between = document.querySelector("#between");
@@ -20,16 +21,23 @@ function guessGame() {
     life--;
     log.innerHTML += guess.value + "<br>";
     if (number === parseInt(guess.value)) {
-      parag.innerHTML = `You are right! Congrats! You knew ${5 - life}th try.`;
-    } else if (number > parseInt(guess.value)) {
+      parag.innerHTML = `🎉🎉You are right! Congrats! You knew ${
+        5 - life
+      }th try.🎉🎉`;
+    } else if (number > parseInt(guess.value) && life != 0) {
       start = parseInt(guess.value);
       parag.innerHTML = `Please try a higher number. You have ${life} life left.`;
       between.innerHTML = `between ${start} - ${stop}`;
-    } else if (number < parseInt(guess.value)) {
+    } else if (number < parseInt(guess.value) && life != 0) {
       stop = parseInt(guess.value);
       parag.innerHTML = `Please try a lower number.You have ${life} life left.`;
       between.innerHTML = `between ${start} - ${stop}`;
     }
+  }
+  if (life == 0) {
+    btn.style.display = "none";
+    resetBtn.style.transform = "scale(1.4)";
+    parag.innerHTML = `Sorry your right is over. Did you like? If you want to play again click Play Again!`;
   }
   guess.value = ``;
 }
